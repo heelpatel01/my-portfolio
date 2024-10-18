60399a119ce228c2ee1ac0f2445f91e004301510
HEEL PATEL <heelpatel216@gmail.com>
2024-10-18 19:59:28 +0530

    Refactor next.config.mjs to add image domains for rendering images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['i.pinimg.com', 'kisan-setu.vercel.app'],
+  },
+}
 
 export default nextConfig;
